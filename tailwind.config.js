1686f54cc2003ad8f0d6d56568f6bc79811622f6
unknown <irsanmaulidan21@gmail.com>
2023-09-08 08:09:20 +0700

    website portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center : true,
+      padding : '20px'
+    },
+    extend: {
+      colors: {
+        primary: '#0ea5e9',
+        secondary : '#64748b',
+        dark: '#020617',
+        third: '#94a3b8',
+        forth: '#0f172a'
+      },
+      screens: {
+        '2xl': '1320px'
+      }
+    },
+  },
+  plugins: [],
+}
+
